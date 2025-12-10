f1e86e3c680008431d086911648db9c0226c2618
Nate Faber <nathaniel@njfaber.com>
2025-12-10 16:38:48 +0100

    reset todays changes

---
+++ nextjs/eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+      "react": reactPlugin,
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    ignores: [".next/**", "node_modules/**"],
+  },
+];
